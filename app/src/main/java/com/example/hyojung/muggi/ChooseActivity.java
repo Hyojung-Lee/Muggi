38786ed1936924094f8219a57db4896f4129dbe3
hyojung <gmolla>
2017-06-04 16:27:46 +0900

    6/4

--- app/src/main/java/com/example/hyojung/muggi/ChooseActivity.java
+++ app/src/main/java/com/example/hyojung/muggi/ChooseActivity.java
@@ -2,6 +2,13 @@ package com.example.hyojung.muggi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class ChooseActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class ChooseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        ImageButton spikey = (ImageButton)findViewById(R.id.spikeySelect);
+        ImageButton baldy = (ImageButton)findViewById(R.id.baldySelect);
+        ImageButton buggy = (ImageButton)findViewById(R.id.buggySelect);
+
+        spikey.setOnClickListener(new ImageButton.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, SpikeySelected.class); // 두번째 액티비티를 실행하기 위한 인텐트
+                startActivity(intent); // 두번째 액티비티를 실행합니다.
+            }
+        });
+        baldy.setOnClickListener(new ImageButton.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, BaldySelected.class); // 두번째 액티비티를 실행하기 위한 인텐트
+                startActivity(intent); // 두번째 액티비티를 실행합니다.
+            }
+        });
+        buggy.setOnClickListener(new ImageButton.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, BuggySelected.class); // 두번째 액티비티를 실행하기 위한 인텐트
+                startActivity(intent); // 두번째 액티비티를 실행합니다.
+            }
+        });
     }
 }
+
+
---
+++ app/src/main/java/com/example/hyojung/muggi/SpikeySelected.java
@@ -0,0 +1,19 @@
+package com.example.hyojung.muggi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class SpikeySelected extends AppCompatActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spikeyselected);
+
+    }
+}
+

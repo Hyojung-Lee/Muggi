dce4ba34360d523fa69c8303c45dbf064fee9c3f
hyojung <gmolla>
2017-06-08 15:03:31 +0900

    6/8

--- app/src/main/java/com/example/hyojung/muggi/EggPlay.java
+++ app/src/main/java/com/example/hyojung/muggi/EggPlay.java
@@ -1,10 +1,6 @@
 package com.example.hyojung.muggi;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Animation;
